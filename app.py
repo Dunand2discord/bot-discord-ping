d8d13be2f95f932348bbca14e5c233f9b3ba2248
Dunand2discord <dunand2discord@outlook.fr>
2025-05-11 10:46:59 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Le bot est en ligne : {bot.user}")
+
+@bot.tree.command(name="bonjour", description="Le bot dit bonjour")
+async def bonjour(interaction: discord.Interaction):
+    latency = round(bot.latency * 1000)
+    await interaction.response.send_message(f"Latence du bot : {latency}ms")
+
+bot.run("LE-TOKEN")  # Remplace par ton vrai token
